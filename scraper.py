b19819cae26ef670d3a68d152f35912c66a0a232
Keith Moss <keith.moss@landgate.wa.gov.au>
2014-11-29 10:46:01 +0800

    Such sausage. Very funny. Wow.

--- scraper.py
+++ scraper.py
@@ -1,23 +1,15 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+from apiclient.discovery import build as discovery_build
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+tableId = '08198754560717916103-08551925598041685897'
+electionKey = 'victorian_election_2014'
+publicAPIKey = 'AIzaSyBuOuavKmg0pKmdGEPdugThfnKQ7v1sSH0'
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+resource = discovery_build('mapsengine', 'v1', developerKey=publicAPIKey)
+features = resource.tables().features().list(
+    id=tableId,
+    version='published',
+    where='has_bbq = 1 OR has_caek = 1 OR has_nothing = 1',
+    maxResults=1000).execute()
+
+scraperwiki.sqlite.save(unique_keys=['election'], data={'election': electionKey, 'count': len(features['features'])})
